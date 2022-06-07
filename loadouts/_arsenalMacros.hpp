3051096b0a19f286759f4dbee33a12f78479737a
Alien314 <stealth131l30@yahoo.com>
2022-06-07 08:32:33 -0500

    More hats, arsenal macros

---
+++ loadouts/_arsenalMacros.hpp
@@ -0,0 +1,11 @@
+#define CUP_HAT(COLOR)\
+	("CUP_H_PMC_Cap_Back_" + #COLOR),\
+	("CUP_H_PMC_Cap_Back_EP_" + #COLOR),\
+	("CUP_H_PMC_Cap_EP_" + #COLOR),\
+	("CUP_H_PMC_Cap_PRR_" + #COLOR),\
+	("CUP_H_PMC_Cap_Back_" + #COLOR)
+
+#define GREENMAG_BELT(CAL)\
+	("greenmag_beltlinked_" + #CAL + "_basic_200"),\
+	("greenmag_beltlinked_" + #CAL + "_basic_100"),\
+	("greenmag_beltlinked_" + #CAL + "_basic_50")
